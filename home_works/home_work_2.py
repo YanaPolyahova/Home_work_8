3fff140f76eaba5dfe8f185ef7c180f0a5db981c
Яна Поляхова <polyahova.yana@mail.ru>
2022-06-10 13:50:16 +0300

    Добавила вторую задачу

--- home_works/home_work_2.py
+++ home_works/home_work_2.py
@@ -1,3 +1,35 @@
+#Tesk_1
+
+month = input("Введите месяц вашего рождения:").lower()
+data = input("Введите дату вашего рождения:")
+if (month == "март" and "21" <= data <= "31") or (month == "апрель" and "1" <= data <= "19"):
+    print("Овен")
+elif (month == "апрель" and "30" >= data >= "21") or (month == "май" and "1" <= data <= "20"):
+    print("Телец")
+elif (month == "май" and "31" >= data >= "21") or (month == "июнь" and "1" <= data <= "20"):
+    print("Близнецы")
+elif (month == "июнь" and "30" >= data >= "21") or (month == "июль" and "1" <= data <= "22"):
+    print("Рак")
+elif (month == "июль" and "31" >= data >= "23") or (month == "август" and "1" <= data <= "22"):
+    print("Лев")
+elif (month == "август" and "31" >= data >= "23") or (month == "сентябрь" and "1" <= data <= "22"):
+    print("Дева")
+elif (month == "сентябрь" and "30" >= data >= "23") or (month == "октябрь" and "1" <= data <= "22"):
+    print("Весы")
+elif (month == "октябрь" and "31" >= data >= "23") or (month == "ноябрь" and "1" <= data <= "21"):
+    print("Скорпион")
+elif (month == "ноябрь" and "30" >= data >= "22") or (month == "декабрь" and "1" <= data <= "21"):
+    print("Стрелец")
+elif (month == "декабрь" and "31" >= data >= "22") or (month == "январь" and "1" <= data <= "19"):
+    print("Козерог")
+elif (month == "январь" and "31" >= data >= "20") or (month == "февраль" and "1" <= data <= "18"):
+    print("Водолей")
+elif (month == "февраль" and "29" >= data >= "19") or (month == "март" and "1" <= data <= "20"):
+    print("Рыбы")
+else:
+    print("Данные введены не корректно. Повторите ввод.")
+
+#Tesk_2
 interest_rate = 10
 location_program = 2
 children_program = 1
